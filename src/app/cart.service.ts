542beb7630d235492fee676df5c8624226168ae6
lucotuslim <limcheeyong1978@gmail.com>
2022-06-20 15:35:17 +0800

    4 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -9,5 +9,21 @@ export class CartService {
   items: Product[] = [];
 
   constructor() { }
+
+
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+  
 }
 
+ 
